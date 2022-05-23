3e9a7f242856b45a456e513f08101e7bb0b4c0c8
Ubuntu <ubuntu@ip-172-31-37-117.ap-northeast-2.compute.internal>
2022-05-23 10:10:36 +0000

    semaphore condv fail

--- threads/synch.c
+++ threads/synch.c
@@ -69,7 +69,10 @@ void sema_down(struct semaphore *sema)
 	old_level = intr_disable();
 	while (sema->value == 0)
 	{
-		list_push_back(&sema->waiters, &thread_current()->elem);
+		// list_push_back(&sema->waiters, &thread_current()->elem);
+		// 수정 : 세마포어
+		list_insert_ordered(&sema->waiters, &thread_current()->elem, cmp_priority, NULL);
+
 		thread_block();
 	}
 	sema->value--;
@@ -113,9 +116,15 @@ void sema_up(struct semaphore *sema)
 
 	old_level = intr_disable();
 	if (!list_empty(&sema->waiters))
+	{
+		// 수정 : 세마포어 - 애매
+		list_sort(&sema->waiters, cmp_sem_priority, NULL);
 		thread_unblock(list_entry(list_pop_front(&sema->waiters),
 								  struct thread, elem));
+	}
 	sema->value++;
+	//수정 : 세마포어
+	thread_yield();
 	intr_set_level(old_level);
 }
 
@@ -289,7 +298,8 @@ void cond_wait(struct condition *cond, struct lock *lock)
 	ASSERT(lock_held_by_current_thread(lock));
 
 	sema_init(&waiter.semaphore, 0);
-	list_push_back(&cond->waiters, &waiter.elem);
+	// list_push_back(&cond->waiters, &waiter.elem);
+	list_insert_ordered(&cond->waiters, &waiter.elem, cmp_sem_priority, NULL);
 	lock_release(lock);
 	sema_down(&waiter.semaphore);
 	lock_acquire(lock);
@@ -310,9 +320,13 @@ void cond_signal(struct condition *cond, struct lock *lock UNUSED)
 	ASSERT(lock_held_by_current_thread(lock));
 
 	if (!list_empty(&cond->waiters))
+	{
+		//  수정 : 세마포어
+		list_sort(&cond->waiters, cmp_sem_priority, NULL);
 		sema_up(&list_entry(list_pop_front(&cond->waiters),
 							struct semaphore_elem, elem)
 					 ->semaphore);
+	}
 }
 
 /* Wakes up all threads, if any, waiting on COND (protected by
@@ -328,4 +342,16 @@ void cond_broadcast(struct condition *cond, struct lock *lock)
 
 	while (!list_empty(&cond->waiters))
 		cond_signal(cond, lock);
+}
+
+bool cmp_sem_priority(const struct list_elem *a, const struct list_elem *b, void *aux UNUSED)
+{
+	struct thread *a_thread = list_entry(a, struct thread, elem);
+	struct thread *b_thread = list_entry(b, struct thread, elem);
+
+	if (a_thread->priority > b_thread->priority)
+	{
+		return 1;
+	}
+	return 0;
 }
